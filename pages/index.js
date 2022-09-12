abca117a609cb6a46a13f3f7ede7d785d8a439e5
tanishksahni <tanishk.sahni2002@gmail.com>
2022-09-12 15:49:02 +0530

    DASH

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className={styles.title}> Apache
         </h1>
 
         <p className={styles.description}>
